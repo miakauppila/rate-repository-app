ee1e60efc5c03a114285705072333af2911a1178
miakauppila <mia.kauppila@gmail.com>
2021-12-13 22:51:47 +0200

    Apollo Client gets authStorage. Implement React context for authStorage

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,14 +1,33 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import Constants from 'expo-constants';
+import { setContext } from '@apollo/client/link/context';
+
+// from app.config
+const { apolloUri } = Constants.manifest.extra;
 
 const httpLink = createHttpLink({
-  // own IP address from Expo dev tools console
-  uri: Constants.manifest.extra.apolloUri,
+  uri: apolloUri,
 });
 
-const createApolloClient = () => {
+const createApolloClient = (authStorage) => {
+  const authLink = setContext(async (_, { headers }) => {
+    try {
+      const accessToken = await authStorage.getAccessToken();
+      return {
+        headers: {
+          ...headers,
+          authorization: accessToken ? `Bearer ${accessToken}` : '',
+        },
+      };
+    } catch (e) {
+      console.log(e);
+      return {
+        headers,
+      };
+    }
+  });
   return new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   });
 };
